8c6307a45f276887d343e15da39d0afe9bcc27f7
Ishtiaque117 <ishtiaque245@gmail.com>
2025-08-29 20:24:50 +0530

    Adding tools for stock price or recent news

--- backend.py
+++ backend.py
@@ -4,20 +4,65 @@ from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_core.tools import tool
 from dotenv import load_dotenv
 import sqlite3
+import requests
+import os
 
 load_dotenv()
 
+ALPHAVANTAGE_API_KEY = os.environ.get("ALPHAVANTAGE_API_KEY")
+
 llm = ChatOpenAI()
+
+# Tools
+search_tool = DuckDuckGoSearchRun(region="us-en")
+
+@tool
+def calculator(first_num: float, second_num: float, operation: str) -> dict:
+    """A simple calculator that can add, subtract, multiply, and divide two numbers."""
+    if operation == "add":
+        return first_num + second_num
+    elif operation == "subtract":
+        return first_num - second_num
+    elif operation == "multiply":
+        return first_num * second_num
+    elif operation == "divide":
+        if second_num == 0:
+            raise ValueError("Cannot divide by zero.")
+        return first_num / second_num
+    else:
+        raise ValueError(f"Unsupported operation: {operation}")
+
+
+@tool
+def get_stock_price(symbol: str) -> dict:
+    """
+    Fetch latest stock price for a given symbol (e.g. 'AAPL', 'TSLA')
+    using Alpha Vantage with API key in the URL.
+    """
+
+    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+    r = requests.get(url)
+    return r.json()
+
+
+tools = [search_tool, calculator, get_stock_price]
+llm_with_tools = llm.bind_tools(tools)
 
 class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
 
 def chat_node(state: ChatState):
+    """ LLM node that may answer or request a tool call."""
     messages = state['messages']
     response = llm.invoke(messages)
     return {"messages": [response]}
+
+tool_node = ToolNode(tools)
 
 connector = sqlite3.connect(database='chatbot.db', check_same_thread=False)
 # Checkpointer
@@ -25,8 +70,12 @@ checkpointer = SqliteSaver(conn=connector)
 
 graph = StateGraph(ChatState)
 graph.add_node("chat_node", chat_node)
+graph.add_node("tools", tool_node)
+
 graph.add_edge(START, "chat_node")
-graph.add_edge("chat_node", END)
+
+graph.add_conditional_edges("chat_node", tools_condition)
+graph.add_edge('tools', "chat_node")
 
 chatbot = graph.compile(checkpointer=checkpointer)
 
